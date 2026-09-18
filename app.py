44487fe4c64b37eed7c175b385b36b4259134081
gggggtgh37-max <gggggtgh37@gmail.com>
2026-09-18 09:13:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def handle_requests():
     server_name = request.args.get("server_name", "").upper()
     key = request.args.get("key")
 
-    if key != "HIMANSHU":
+    if key != "777LIKE":
         return jsonify({"error": "Invalid or missing API key 🔑"}), 403
 
     if not uid or not server_name:
